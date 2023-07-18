819af80f148983acbd52e734657cf520eb3a9df7
hall488 <hall488@purdue.edu>
2023-07-18 18:36:15 -0400

    Add color selection and structured flexboxes
    Added the color options and size the flex boxes where necessary.
    Created a css selector for selecting colors in the future.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 let sketch = document.querySelector('.sketch');
 
+let palette = document.querySelectorAll('.palette div');
+
+palette.forEach(c => {
+    c.style.width = 100 + 'px';
+    c.style.height = 100 + 'px';
+    c.style.backgroundColor = c.getAttribute('data');
+});
 
 function createGrid(n) {
     for(i = 0; i < n*n; i ++) {
