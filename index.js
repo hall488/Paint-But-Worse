f34eef5febb7825323aa7404655cd7b66871ed51
hall488 <hall488@purdue.edu>
2023-07-18 18:41:32 -0400

    Add resizing button function
    Resize button will now prompt user for a value to create a new grid.
    Value must be between 0 and 100.

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ let sketch = document.querySelector('.sketch');
 
 let palette = document.querySelectorAll('.palette div');
 
+let resize = document.querySelector('.resize');
+
+resize.addEventListener('click', () => {
+    let val;
+    for(;;) {
+        val = prompt("Enter n for a n by n grid!");
+        if(val > 0 && val < 101) break;
+        console.log("Must be between 0 and 100!");
+    }
+    sketch.innerHTML = '';
+    createGrid(val);
+});
+
 palette.forEach(c => {
     c.style.width = 100 + 'px';
     c.style.height = 100 + 'px';
